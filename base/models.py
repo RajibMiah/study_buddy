1a80b078c55d5d4de6c4b3ff1d768313b7c2a426
rajibmiah <ahmedrajib942@gmail.com>
2022-02-08 11:04:45 +0600

    room update feature added

--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.shortcuts import render
 
 class Topic(models.Model):
     name = models.CharField(max_length=255)
@@ -33,3 +34,4 @@ class Message(models.Model):
     
     def __str__(self) -> str:
         return self.body[0:50]
+
--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from django.urls import path
-from base.views import home , room , createRoom
+from base.views import home , room , createRoom , updateRoom
 urlpatterns = [
     path('', home , name = 'home'),
     path('room/<str:pk>/' , room , name = 'room'),
-    path('create-room' , createRoom  , name ='create-room' )
+    path('create-room/' , createRoom  , name ='create-room' ),
+    path('update-room/<str:pk>/' , updateRoom , name = 'update-room')
 ]
