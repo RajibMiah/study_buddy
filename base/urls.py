7f91f2256f20b11cfa4e3015e958e22dbe674b52
rajibmiah <ahmedrajib942@gmail.com>
2022-02-08 11:33:09 +0600

    room delete option added

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
-from unicodedata import name
+
 from django.urls import path
-from base.views import home , room , createRoom , updateRoom
+from base.views import deleteRoom, home , room , createRoom , updateRoom
 urlpatterns = [
     path('', home , name = 'home'),
     path('room/<str:pk>/' , room , name = 'room'),
     path('create-room/' , createRoom  , name ='create-room' ),
-    path('update-room/<str:pk>/' , updateRoom , name = 'update-room')
+    path('update-room/<str:pk>/' , updateRoom , name = 'update-room'),
+    path('delete-room/<str:pk>/' , deleteRoom , name = 'delete-room' )
 ]
--- base/views.py
+++ base/views.py
@@ -45,3 +45,13 @@ def updateRoom(request , pk):
     context = {'form':form}
 
     return render(request , 'base/room_form.html' , context)
+
+
+def deleteRoom(request , pk):
+    room = Room.objects.get(id = pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+        
+    context = {'obj':room}
+    return render(request , 'base/delete.html' , context)
