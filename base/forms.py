9c0fef5fc1c323cfb1435cbe15ccf234719e138e
rajibmiah <ahmedrajib942@gmail.com>
2022-02-13 23:12:24 +0600

    exclude host and participants and also setup static files

--- base/forms.py
+++ base/forms.py
@@ -2,7 +2,9 @@
 from django.forms import ModelForm
 from .models import Room
 
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['host', 'participants']
