5381eb056cc16a1e7c3e3cef48566f099dce95ce
rajibmiah <ahmedrajib942@gmail.com>
2022-02-21 16:34:53 +0600

    worked on user updated client html

--- base/views.py
+++ base/views.py
@@ -201,9 +201,10 @@ def deleteMessage(request, pk):
     context = {'obj': message}
     return render(request, 'base/delete.html', context)
 
-
+@login_required(login_url='/login')
 def updateUser(request):
-    return HttpResponse('user profile update page')
+    context = {}
+    return render(request , 'base/update_user.html' ,context)
 
 
 def topicsPage(request):
