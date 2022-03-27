e2476cf63691c629d6ba3a3e91ef46d934922080
rajibmiah <ahmedrajib942@gmail.com>
2022-03-27 16:34:18 +0600

    changed css style

--- base/models.py
+++ base/models.py
@@ -1,7 +1,18 @@
 
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.contrib.auth.models import AbstractUser
+
+# class User(AbstractUser):
+#     name = models.CharField(max_length=255 , null=True)
+#     email = models.EmailField(unique=True , null=True)
+#     bio = models.CharField(null=True)
+
+#     avator = models.ImageField(null=True ,default = 'avatar.svg')
+#     # USERNAME_FIELD ='email'
+#     # REQUIRED_FIELDS = []
 
 class Topic(models.Model):
     name = models.CharField(max_length=255)
--- base/views.py
+++ base/views.py
@@ -20,18 +20,21 @@ def login(request):
         return redirect('home')
 
     if request.method == 'POST':
-        email = request.POST.get('email').lower()
+        username = request.POST.get("username")
+        # email = request.POST.get('email').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(email=email)
+            # pass
+            # user = User.objects.get(email=email)
+            user = User.objects.get(username = username)
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, email=email, password=password)
-
+        # user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username = username , password = password)
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
             return redirect('home')
         else:
             messages.error(request, 'Username OR password does not exit')
