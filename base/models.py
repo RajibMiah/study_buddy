78d735fbe51f069f88599b87cffc9df62c513216
rajibmiah <ahmedrajib942@gmail.com>
2022-03-27 17:25:05 +0600

    updated

--- base/models.py
+++ base/models.py
@@ -45,7 +45,7 @@ class Message(models.Model):
     created  = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-updated' , '-created']
+        ordering = ['updated' , 'created']
 
     def __str__(self) -> str:
         return self.body[0:50]
