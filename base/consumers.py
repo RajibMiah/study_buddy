a5b992b5ec1a860b6db36245ace49102de7f1d88
rajibmiah <ahmedrajib942@gmail.com>
2022-06-09 02:07:45 +0600

    update

--- base/consumers.py
+++ base/consumers.py
@@ -2,8 +2,9 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.core.serializers.json import DjangoJSONEncoder
 
-from .models import Message, Room
+from .models import Message, Room, User
 
 
 class RoomConsumer(WebsocketConsumer):
@@ -13,6 +14,7 @@ class RoomConsumer(WebsocketConsumer):
         self.room_group_name = None
         self.room = None
         self.user = None  # new
+        self.user_details = None
 
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_id']
@@ -36,7 +38,6 @@ class RoomConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        print('text', text_data_json)
         message = text_data_json['message']
 
         if not self.user.is_authenticated:  # new
@@ -48,19 +49,42 @@ class RoomConsumer(WebsocketConsumer):
         self.message_data = Message.objects.filter(
             room_id=self.room).values().last()
 
-        self.message_data['type'] = 'chat_message'
-        # self.message_data['updated'] = str(self.message_data.update.utcnow())
-        # self.message_data['created'] = str(self.message_data.created.utcnow())
+        print('user id', self.message_data["user_id"])
 
-        print(self.message_data)
+        self.user_details = User.objects.filter(
+            id=self.message_data["user_id"]).values().first()
+
+        print(self.user_details)
+
+        # print(json.dumps(
+        #     self.user_details, indent=4, sort_keys=True,   cls=DjangoJSONEncoder))
+
+        self.message_data['type'] = 'chat_message'
+
+        self.message_data['updated'] = json.dumps(
+            self.message_data["updated"], indent=4, sort_keys=True,   cls=DjangoJSONEncoder)
+        self.message_data['created'] = json.dumps(
+            self.message_data["created"], indent=4, sort_keys=True,   cls=DjangoJSONEncoder)
+
+        # self.user_obj = dict()
+        # for key, value in self.user_details.items:
+        #     if json.dumps(self.message_data[key], indent=4, sort_keys=True,   cls=DjangoJSONEncoder):
+
+        #         self.user_obj[key] = value
+        #     else:
+        #         self.user_obj[key] = json.dumps(
+        #             self.message_data[key], indent=4, sort_keys=True,   cls=DjangoJSONEncoder)
+        # print(self.user_obj)
+        # self.message_data['user'] = self.user_details
+
+        # print(self.message_data)
+        # print(type(self.message_data))
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
-            # self.message_data
+            self.message_data
         )
-        # Message.objects.create(
-        #     user=self.user, room=self.room, body=message)  # new
 
     def chat_message(self, event):
-        print(" prinfing event object", event)
+        # print(" prinfing event object", event)
         self.send(text_data=json.dumps(event))
