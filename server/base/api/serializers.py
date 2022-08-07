72f18de3a567efee63af6992162464102bec2f04
rajibmiah <ahmedrajib942@gmail.com>
2022-08-07 09:37:56 +0600

    profile pic issue is resolved

--- server/base/api/serializers.py
+++ server/base/api/serializers.py
@@ -1,22 +1,29 @@
 
-from base.models import Room, Topic, User
+from base.models import Room, Topic
 from rest_framework import serializers
 
 # from stripe import Source
+
+AVATOR_BASE_URL = 'http://127.0.0.1:8000/images/'
 
 
 class SimpleUserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(read_only=True)
-    # avator = serializers.HyperlinkedRelatedField(
-    #     many=True, read_only=True, view_name='avator', allow_null=True)
+    username = serializers.CharField(read_only=True)
     uuid = serializers.UUIDField(read_only=True)
+    avator = serializers.SerializerMethodField(read_only=True)
+
+    def get_avator(self, user):
+        if user.avator:
+            avator_url = str(AVATOR_BASE_URL) + str(user.avator)
+            return avator_url
+        return None
 
 
 class SimpleTopicSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(read_only=True)
-    username = serializers.CharField(read_only=True)
 
 
 class RoomSerializer(serializers.ModelSerializer):
@@ -33,10 +40,8 @@ class RoomSerializer(serializers.ModelSerializer):
     def get_participants(self, data):
         room = Room.objects.prefetch_related('participants').get(pk=data.pk)
         room_participants = room.participants.all()
-        # request=self.request
-        return SimpleUserSerializer(room_participants, many=True).data
 
-    # def get_owner(self, data):
+        return SimpleUserSerializer(room_participants, many=True).data
 
 
 class TopicSerializer(serializers.ModelSerializer):
